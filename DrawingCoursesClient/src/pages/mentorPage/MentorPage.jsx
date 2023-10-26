0667c855b4705ea8d74b02b5cb5ebf4a586a2272
justDaniel2002 <todentsukanai@gmail.com>
2023-10-26 22:03:47 +0700

    update mentor page

--- DrawingCoursesClient/src/pages/mentorPage/MentorPage.jsx
+++ DrawingCoursesClient/src/pages/mentorPage/MentorPage.jsx
@@ -1,54 +1,70 @@
+import { Link } from "react-router-dom";
 import { coursesData } from "../../data/data";
 
 export const MentorPage = () => {
   return (
     <>
-      <div className="border-2 rounded-xl">
-        <div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10">
-          <table class="table-auto">
-            <thead>
-              <tr>
-                <th class="px-4 py-2 text-emerald-600">Course</th>
-                <th class="px-4 py-2 text-emerald-600">Image</th>
-                <th class="px-4 py-2 text-emerald-600">Description</th>
-                <th class="px-4 py-2 text-emerald-600">Price</th>
-                <th class="px-4 py-2 text-emerald-600">Category</th>
-                <th class="px-4 py-2 text-emerald-600">CreateDate</th>
-                <th class="px-4 py-2 text-emerald-600"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {coursesData.map((course) => (
-                <tr>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    {course.course}
-                  </td>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    <img src={course.imageSrc}/>
-                  </td>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    {course.profession}
-                  </td>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    {course.price}
-                  </td>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    {course.category}
-                  </td>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    {course.createdDate}
-                  </td>
-                  <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    <button className="block mb-3 p-2 bg-buttonBlue text-white rounded-xl">Detail</button>
-                    <button className="block mb-3 p-2 bg-buttonBlue text-white rounded-xl">Edit</button>
-                    <button className="block mb-3 p-2 bg-red text-white rounded-xl">Delete</button>
-                  </td>
-                </tr>
-              ))}
-              
-            </tbody>
-          </table>
+      <div className="bg-buttonBlue text-white pt-10 px-20">
+        <div className="flex justify-between mb-5">
+          <div className="text-xl font-light">Instructor Dashboard</div>
+          <div className="flex">
+            <button className="p-2 text-buttonBlue bg-white rounded-2xl hover:text-white hover:bg-buttonBlue mr-10">
+              Order history
+            </button>
+            <button className="p-2 text-buttonBlue bg-white rounded-2xl hover:text-white hover:bg-buttonBlue">
+              Create a new course
+            </button>
+          </div>
         </div>
+        <div className="flex">
+          <Link className="pb-3 mr-20" to={"/"}>
+            Courses
+          </Link>
+          <Link className="pb-3" to={"/"}>
+            Review
+          </Link>
+        </div>
+      </div>
+      <div className=" py-10 px-20 flex">
+        <div className="w-1/4">
+          Total Revenue
+          <div>$0</div>
+        </div>
+        <div className="w-1/4">
+          Total Students
+          <div>0</div>
+        </div>
+      </div>
+
+      <div className="my-10 px-60">
+        <input className="p-2 border border-grey500" placeholder="Search course" />
+        <button className="p-2 bg-grey500">Search</button>
+      </div>
+
+      <div className="px-40">
+        {coursesData.map((course) => (
+          <>
+            <div className="flex mb-10 p-5 border border-grey500 justify-between">
+              <img className="w-1/4 mr-10" src={course.imageSrc} />
+              <div className="mr-10 flex flex-col justify-between">
+                <div className="text-2xl font-semibold">{course.course}</div>
+                <div className="text-sm">
+                  {course.profession} - {course.price}$
+                </div>
+                <div className="font-thin mb-5">{course.category}</div>
+                <div className="text-right">{course.createdDate}</div>
+              </div>
+              <div className="self-center">
+                <button className="block mb-3 p-2 bg-buttonBlue text-white rounded-xl">
+                  Edit
+                </button>
+                <button className="block mb-3 p-2 bg-red text-white rounded-xl">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
